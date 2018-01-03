cc6d585d2c3345decb4f424cae2969473a50948d
Connor Yanz <connor.yanz@gmail.com>
2018-01-02 19:04:19 -0500

    Completes frontend  root and App files

--- frontend/mindbind.jsx
+++ frontend/mindbind.jsx
@@ -1,14 +1,21 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as APIUtil from './util/session_api_util';
+import Root from './components/root';
+// import * as APIUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = APIUtil.login;
-  window.logout = APIUtil.logout;
-  window.signup = APIUtil.signup;
-
+  // window.login = APIUtil.login;
+  // window.logout = APIUtil.logout;
+  // window.signup = APIUtil.signup;
+  const store = configureStore();
+  // Testing below
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // Testing above
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>MindBind</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
+
 });

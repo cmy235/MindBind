e03ac336a7f465c55c340eb3c10765ca457c8705
Connor Yanz <connor.yanz@gmail.com>
2018-02-20 23:50:59 -0500

    Fix logo, resize, add styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -41,78 +41,78 @@ class Navbar extends React.Component{
       <div className="main-nav-current-user">
         <div className="header-wrapper">
           <h2 className="headerName">Hello, {parsedName}!</h2>
-            <button className="logout-button"
-              onClick={this.deployLogout}
-              >Log Out</button>
-          </div>
-          <Link to="/decks">
-            <ul className="logo-container">
-              <li className="logo">
-                <img className="logo-img" src={window.img.logo}></img>
-              </li>
-              <li className="mindbind-title-one">
-                MIND
-              </li>
-              <li  className="mindbind-title-two">
-                BIND
-              </li>
-            </ul>
-          </Link>
+          <button className="logout-button"
+            onClick={this.deployLogout}
+            >Log Out</button>
         </div>
-      );
-    }
-
-    sessionLinks (){
-      return(
-        <div>
-          <div className="background-img">
-            <nav className="main-nav">
-              <a href="/decks/">
-                <ul className="logo-container">
-                  <li>
-                    <img src={window.img.head}></img>
-                  </li>
-                  <li className="mindbind-title-one">
-                    MIND
-                  </li>
-                  <li  className="mindbind-title-two">
-                    BIND
-                  </li>
-                </ul>
-              </a>
-
-              <div className="login-signup-container">
-                <button className="login-button"
-                  onClick={this.props.openModal1}
-                  >Login</button>
-                <button
-                  className="signup-button"
-                  onClick={this.props.openModal2}
-                  >Get Started</button>
-
-              </div>
-            </nav>
-          </div>
-          <div className="greeting">The World's Best Flashcards</div>
-          <div className="bottom-boxes">
-            <div className="box-1"
-              onClick={this.props.openModal2}>
-              Browse Flashcards
-            </div>
-            <div className="box-2"
-              onClick={this.props.openModal2}
-              >Make Flashcards</div>
-            <div className="box-3"
-              onClick={this.props.openModal2}
-              >Make Me Smarter!</div>
-          </div>
-        </div>
-      );
-    }
+        <Link to="/decks">
+          <ul className="logo-container">
+            <li className="logo">
+              <img className="logo-img" src={window.img.logo}></img>
+            </li>
+            <li className="mindbind-title-one">
+              MIND
+            </li>
+            <li  className="mindbind-title-two">
+              BIND
+            </li>
+          </ul>
+        </Link>
+      </div>
+    );
   }
 
-  export default Navbar;
+  sessionLinks (){
+    return(
+      <div>
+        <div className="background-img">
+          <nav className="main-nav">
+            <a href="/decks/">
+              <ul className="logo-container">
+                <li className="logo">
+                  <img className="logo-img" src={window.img.logo}></img>
+                </li>
+                <li className="mindbind-title-one">
+                  MIND
+                </li>
+                <li  className="mindbind-title-two">
+                  BIND
+                </li>
+              </ul>
+            </a>
 
-  {/*
-    this.props.currentUser.username
-    */}
+            <div className="login-signup-container">
+              <button className="login-button"
+                onClick={this.props.openModal1}
+                >Login</button>
+              <button
+                className="signup-button"
+                onClick={this.props.openModal2}
+                >Get Started</button>
+
+            </div>
+          </nav>
+        </div>
+        <div className="greeting">The World's Best Flashcards</div>
+        <div className="bottom-boxes">
+          <div className="box-1"
+            onClick={this.props.openModal2}>
+            Browse Flashcards
+          </div>
+          <div className="box-2"
+            onClick={this.props.openModal2}
+            >Make Flashcards</div>
+          <div className="box-3"
+            onClick={this.props.openModal2}
+            >Make Me Smarter!</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
+
+{/*
+  this.props.currentUser.username
+  */}
